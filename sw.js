5921bdccea9cb4a8c8ef3ff9f78aa4470965d7fa
RamonRalf <ramonralf0@outlook.com>
2026-05-18 16:38:11 -0300

    Add basic service worker with install and fetch events

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (e) => {
+    console.log('Service Worker Instalado');
+});
+
+self.addEventListener('fetch', (e) => {
+    // Apenas deixa a requisição passar normalmente
+});
+
